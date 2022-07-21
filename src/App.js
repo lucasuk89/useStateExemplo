3168509cd837e89ff035426752cc68dfece2a411
Lucas Ferraz Martins <lucasuk89@gmail.com>
2022-07-21 17:03:06 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+
+  const [title, setTitle] = useState("Hello world 3");
+  const [showContent, setShowContent] = useState(false);
+
+  function handleTitle() {
+    setTitle("O estado foi alterado");
+  }
+
+  function handleContent() {
+    setShowContent(!showContent)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{title}</h1>
+      <ExclusiveContent showContent={showContent} />
+      <button onClick={handleContent}>{showContent ? 'Clique para esconder o conteudo' : 'Clique para exibir o conteudo'}</button>
+      &nbsp;
+      <button onClick={handleTitle}>Alterar o titulo </button>
     </div>
   );
 }
+
+function ExclusiveContent(props) {
+  return (
+    <>
+      {props.showContent && <h2>CONTEUDO DESCOBERTO</h2>}
+    </>
+  )
+}
+
+
 
 export default App;
